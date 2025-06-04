9dd4027e47226197ac6a0c271b91a7cb4c08d3cf
Martin Kirov <fas7blas7@gmail.com>
2025-06-04 18:39:08 +0300

    Adding HouseParty task

---
+++ 02.HouseParty/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace P02_HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guestList = new List<string>();
+
+            // 1. Read Input
+            int n = int.Parse(Console.ReadLine());
+
+            // 2. Process Logic
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentCmd = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                // Defensive Programming Rule: Check if the array is not null and check if there are any elements inside (Length > 0)
+                string name = currentCmd[0];
+
+                if (currentCmd.Length == 3)
+                {
+                    // "{name} is going" command
+                    if (guestList.Contains(name))
+                    {
+                        // We will enter here only if guestList contains name inside
+                        Console.WriteLine($"{name} is already in the list!");
+                        continue; // Go directly to the next command after error
+                    }
+
+                    guestList.Add(name);
+                }
+                else if (currentCmd.Length == 4)
+                {
+                    // "{name} is not going" command
+                    if (!guestList.Contains(name))
+                    {
+                        // We will enter here only if guestList does not contain name inside
+                        Console.WriteLine($"{name} is not in the list!");
+                        continue; // Go directly to the next command after error
+                    }
+
+                    guestList.Remove(name);
+                }
+            }
+
+            // 3. Print Output
+            foreach (string name in guestList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
